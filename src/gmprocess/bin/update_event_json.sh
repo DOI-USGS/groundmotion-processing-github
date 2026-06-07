4b579e53a485c46740b99cf5f2465561473ef122
Eric Thompson <emthompson@usgs.gov>
2026-06-07 06:45:39 -0600

    Delete src/gmprocess directory

--- src/gmprocess/bin/update_event_json.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for efile in `find . -name event.json`; do
-    sed -e 's/"lat":/"latitude":/g; s/"lon":/"longitude":/g; s/"depth":/"depth_km":/g; s/"mag_type":/"magnitude_type":/g' $efile > tmp && mv tmp $efile
-done
